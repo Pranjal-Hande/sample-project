ecb48f0fb58ed24c9212bb31f2a94e862415664b
Pranjal-Hande <pranjalhande2002@gmail.com>
2022-10-08 11:01:59 +0530

    first commit

---
+++ inputfunctions/src/inputfunctions/Inputfunctions.java
@@ -0,0 +1,17 @@
+//Basic Operations on Numbers
+package inputfunctions;
+public class Inputfunctions  
+{  
+	public static void main(String args[])   
+	{  
+		int n1 = 225, n2 = 115, sum, diff, mult, div;  
+		sum = n1 + n2;
+		diff = n1 - n2;
+		mult = n1 * n2;
+		div = n1 / n2;
+		System.out.println("The addition of numbers is: "+sum);
+		System.out.println("The difference of numbers is: "+diff);
+		System.out.println("The multiplication of numbers is: "+mult);
+		System.out.println("The division of numbers is: "+div);
+	}  
+}  
